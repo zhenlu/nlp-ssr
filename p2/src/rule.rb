401e4a6e8ab0d763f471c1b54afdd2ec4928734c
Zhen Lu <ne.rwa.lz@gmail.com>
2012-02-02 16:46:06 -0800

    finish rule.rb

--- p2/src/rule.rb
+++ p2/src/rule.rb
@@ -8,9 +8,47 @@
 
 class Rule
   def initialize
+    # read the lexicon
+    @dict = Hash.new
+    File.open("data/lexicon.dat") do |f|
+      while line = f.gets
+        line = line.split(':')
+        dict = line[1].split(',')
+        @dict[line[0]] = dict
+      end
+    end
+    # read the grammar rules
+    @grammar = Hash.new
+    File.open("data/grammar.dat") do |f|
+      while line = f.gets
+        line = line.split(':')
+        grammar = line[1].split('|')
+        @grammar[line[0]] = grammar
+      end
+    end
   end
   
   def expand(token)
+    if token.class == GrammarToken
+      resultlist = @grammar[token.token]
+      grammar = resultlist[rand(resultlist.size)].split
+      grammar.reverse
+      result = []
+      grammar.each do |i|
+        result.push(create_token i)
+      end
+      result
+    else
+      wordlist = @dict[token.token]
+      result = WordToken.new wordlist[rand(wordlist.size)]
+    end
+  end
+  
+  def create_token(token)
+    if ["P", "N", "PN", "V", "ADJ", "DET", "AUX", "COM"].include? token
+      LexiconToken.new token
+    else
+      GrammarToken.new token
+    end
   end
 end
-
